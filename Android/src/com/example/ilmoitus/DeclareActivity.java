a74d41b5bad0c93d2b9888648d58dfba7e316066
willemhustinx <willemhustinx@gmail.com>
2014-04-22 23:01:19 +0200

    androiid added view for declaration details
    TODO
    
    - standaart style voor knoppen maken
    - iets maken waardoor de knoppen onderaan niet in elke activity
    aangemaakt moeten worden
    - iets verzinnen om een lijst met declaraties weer te geven in een
    scrollview en die dynamische te kunne bijwerken
    - date picker die niet te groot is

--- Android/src/com/example/ilmoitus/DeclareActivity.java
+++ Android/src/com/example/ilmoitus/DeclareActivity.java
@@ -2,10 +2,11 @@ package com.example.ilmoitus;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class DeclareActivity extends Activity {
 	
@@ -14,11 +15,25 @@ public class DeclareActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_declare);
 		
-		Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+		Spinner dropdown = (Spinner)findViewById(R.id.spinner);
 		String[] items = new String[]{"1", "2", "three"};
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
 		dropdown.setAdapter(adapter);
 	
+	}
+
+	public void onButtonClickAddItem(View view) {	
+		Toast.makeText(getApplicationContext(), "add Item" , Toast.LENGTH_SHORT).show();
+		
+		Intent intent = new Intent(DeclareActivity.this, DeclareItemActivity.class);
+		startActivity(intent);
+	}
+	
+	public void onButtonClickSubmit(View view) {	
+		Toast.makeText(getApplicationContext(), "On Submit button Clicked" , Toast.LENGTH_SHORT).show();
+		
+		Intent intent = new Intent(DeclareActivity.this, MainActivity.class);
+		startActivity(intent);
 	}
 }
 
