8a7b6fdb1adb100131dc12f92ede390fde2ba7d3
abolhuis88 <alexander.bolhuis@freshheads.com>
2014-04-22 08:12:00 +0200

    Added empty loginview and empty detailview

---
+++ iOS/Ilmoitus/Ilmoitus/DeclarationDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeclarationDetailViewController.h
+//  Ilmoitus
+//
+//  Created by Alexander Bolhuis on 22-04-14.
+//  Copyright (c) 2014 42IN12EWa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeclarationDetailViewController : UIViewController
+
+@end
---
+++ iOS/Ilmoitus/Ilmoitus/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Ilmoitus
+//
+//  Created by Alexander Bolhuis on 22-04-14.
+//  Copyright (c) 2014 42IN12EWa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
