f4bbc81249331b69713fd3627070f1bc64e8e234
abolhuis88 <alexander.bolhuis@freshheads.com>
2014-05-09 13:32:34 +0200

    Constants

---
+++ iOS/Ilmoitus/Ilmoitus/constants.h
@@ -0,0 +1,16 @@
+//
+//  constants.h
+//  Ilmoitus
+//
+//  Created by Alexander Bolhuis on 09-05-14.
+//  Copyright (c) 2014 42IN12EWa. All rights reserved.
+//
+
+#ifndef Ilmoitus_constants_h
+#define Ilmoitus_constants_h
+
+#define baseURL @"http://2.sns-ilmoitus.appspot.com"
+
+
+
+#endif
