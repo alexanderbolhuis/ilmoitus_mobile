c2692e4ae0682ab2a41c38256f4cf62b7a92a2c2
abolhuis88 <alexander.bolhuis@freshheads.com>
2014-05-14 09:55:38 +0200

    ST03-11 Get supervisorlist

--- iOS/Ilmoitus/Ilmoitus/NewDeclarationViewController.m
+++ iOS/Ilmoitus/Ilmoitus/NewDeclarationViewController.m
@@ -8,10 +8,12 @@
 
 #import "NewDeclarationViewController.h"
 #import "Declaration.h"
+#import "Supervisor.h"
+#import "constants.h"
 
 @interface NewDeclarationViewController ()
-@property (weak, nonatomic) IBOutlet UIPickerView *supervisorList;
 @property (weak, nonatomic) IBOutlet UITextField *supervisor;
+@property (weak, nonatomic) NSMutableArray *supervisorList;
 @end
 
 @implementation NewDeclarationViewController
@@ -23,6 +25,7 @@
     
     // Create new blank Declaration
     Declaration *newDeclaration = [[Declaration alloc] init];
+    [self getSupervisorList];
 
 }
 
@@ -32,4 +35,46 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)getSupervisorList
+{
+    // Do Request
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    [manager.requestSerializer setValue:[[NSUserDefaults standardUserDefaults] stringForKey:@"token"] forHTTPHeaderField:@"Authorization"];
+    NSString *url = [NSString stringWithFormat:@"%@/supervisors/", baseURL];
+    [manager GET:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSError* error;
+        NSDictionary* json = [NSJSONSerialization
+                              JSONObjectWithData:responseObject
+                              
+                              options:kNilOptions
+                              error:&error];
+        
+        NSLog(@"%@", json);
+        
+        NSMutableArray *supervisorsFound = [[NSMutableArray alloc] init];
+        for (NSDictionary *supervisor in json) {
+            Supervisor *sup = [[Supervisor alloc] init];
+            sup.class_name = supervisor[@"class_name"];
+            sup.first_name = supervisor[@"first_name"];
+            sup.last_name = supervisor[@"last_name"];
+            sup.email = supervisor[@"email"];
+            sup.employee_number = [supervisor[@"employee_number"] integerValue];
+            sup.department = [supervisor[@"department"] longLongValue];
+            sup.supervisor = [supervisor[@"supervisor"] longLongValue];
+            sup.max_declaration_price = [supervisor[@"max_declaration_price"] floatValue];
+            
+            [supervisorsFound addObject:sup];
+        }
+        _supervisorList = supervisorsFound;
+        
+        // TODO werkende dropdown maken
+        Supervisor *sup = [_supervisorList firstObject];
+        _supervisor.text = [NSString stringWithFormat:@"%@ %@", sup.first_name, sup.last_name];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+
+}
 @end
---
+++ iOS/Ilmoitus/Ilmoitus/Supervisor.m
@@ -0,0 +1,13 @@
+//
+//  Supervisor.m
+//  Ilmoitus
+//
+//  Created by Alexander Bolhuis on 14-05-14.
+//  Copyright (c) 2014 42IN12EWa. All rights reserved.
+//
+
+#import "Supervisor.h"
+
+@implementation Supervisor
+
+@end
